842a287f2f08aa9fd1a073c15502c156533abe21
maxxrdrgz <maxxrdrgz@gmail.com>
2014-02-19 16:32:20 -0700

    print update

--- print.c
+++ print.c
@@ -10,7 +10,7 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
     
     if (++line_count > MAX_LINES_PER_PAGE)
     {
-        /* Missing Code Here */
+        print_page_header(source_name_to_print[], date[]);
     }
     if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
     {
@@ -28,7 +28,7 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
 }
 static void print_page_header(char source_name[], char date[])
 {
-    static int page_number = 0;
+    static int page_number = 1;
     
     /* Missing Code Here */
 }
