bddba538586d28a2263fc31a9406ed4d4bf83b2f
calope0106 <clopez.1994@yahoo.com>
2014-02-23 21:19:44 -0800

    Update common.h

--- common.h
+++ common.h
@@ -1,20 +1,20 @@
 #ifndef CSE220_project2_common_h
 #define CSE220_project2_common_h
-
+#**********************************************************************
 #include <stdio.h>
 #include <sys/types.h>
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+#**********************************************************************
 #define FORM_FEED_CHAR          '\f'
 #define MAX_FILE_NAME_LENGTH    32
 #define MAX_SOURCE_LINE_LENGTH  256
 #define MAX_PRINT_LINE_LENGTH   80
 #define MAX_LINES_PER_PAGE      50
 #define DATE_STRING_LENGTH      26
-
+#*********************************************************************
 typedef enum {
     FALSE, TRUE,
 }BOOLEAN;
