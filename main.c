14796d51e9e7c34f14eafe46a1597592d7d0f593
calope0106 <clopez.1994@yahoo.com>
2014-02-23 22:13:36 -0800

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,9 @@ int main (int argc, const char *argv[])
 {
     FILE *source_file;						
     char source_name[MAX_FILE_NAME_LENGTH];	//Array for incoming source code 
-    char date[DATE_STRING_LENGTH];
-    source_file = init_lister(*argv, source_name , date );	
-    int counter = 0;
+    char date[DATE_STRING_LENGTH];		//Array for date
+    source_file = init_lister(*argv, source_name , date );	//source file formatted in init_lister	
+    int counter = 0;	//counter initialized to zero
 
     while(get_source_line(source_file, source_name, date)) //repeats until null terminator
     {
@@ -14,30 +14,30 @@ int main (int argc, const char *argv[])
     }
     
    /* Missing Code Here */
-    return 0;
+    return 0; //return zero when loop has exited
 }
 FILE *init_lister(const char *name, char source_file_name[], char dte[])	//formats incoming source code
 {
     FILE *file;
-    char source_name = *name;
-    char date[26];
-    int i = 0;
+    char source_name = *name;	//pointer
+    char date[26];	//sets up date capacity
+    int i = 0;		//intialized to 0 for the first element in array
     int x = 10;
     
     //Sets up timer and date
     time_t timer = time(0);		
     struct tm *timevar = localtime(&timer);
     char* itoa(*timevar, date, x);
-    while(date[i] != '\0')
+    while(date[i] != '\0')	//loops to print date and time
     {
     	dte[i] = date[i];
     	i++;
     }
-    dte[i] = '\0';
+    dte[i] = '\0'; //adds a null terminator once it has finished printing
     
     //printf("%s", ctime(&timer));
     FILE *f = fopen(source_file_name, "w");
-    printf("%s", dte);
+    printf("%s", dte);	//print date
 
 
     /* Missing Code Here */
@@ -52,9 +52,9 @@ BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])	//R
     if (source_buffer[line_number] != '\0') //This is missing a condition. Prints until the null terminator
     {
 	source_buffer[line_number];	
-	line_number++;	
-	sprintf(line_number, '%d %s', source_buffer[line_number]);
-	print_line(line_number, src_name, todays_date );
+	line_number++;	//increment
+	sprintf(line_number, '%d %s', source_buffer[line_number]); //print source code
+	print_line(line_number, src_name, todays_date );	//print arguements according to print_line
 	
       return (TRUE);	//returns 1 to continue looping
     }
